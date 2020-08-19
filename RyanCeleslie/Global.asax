595a2ebc1fae98a57d288139f4e562b219a6d13c
Ryan Celeslie <celeslie@gmail.com>
2020-08-18 22:45:42 -0400

    initial commit

---
+++ RyanCeleslie/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RyanCeleslie.MvcApplication" Language="C#" %>
---
+++ RyanCeleslie/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RyanCeleslie.Startup))]
+namespace RyanCeleslie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
