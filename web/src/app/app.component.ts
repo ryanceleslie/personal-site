b3e407ffb721d6de0bf918da32272f1e189d0bd5
Ryan Celeslie <ryan.celeslie@outlook.com>
2024-03-21 13:23:26 -0400

    added font-awesome libraries

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faBriefcase, faUniversity } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'web';
+  title = 'Ryan Celeslie';
+  constructor(library: FaIconLibrary) {
+    library.addIcons(
+      faBriefcase,
+      faUniversity
+    );
+  }
 }
